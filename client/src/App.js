807e0a72219ec28aa80dacfbd6391a121fa3a8cb
taynguyen528 <pttnguyen528@gmail.com>
2023-12-26 20:07:05 +0700

    push code client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Button, Typography } from "@mui/material";
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>MondayProject</h1>
+      <Button variant="contained" color="primary">
+        Material UI
+      </Button>
+    </div>
+  );
+}
+
+export default App;
